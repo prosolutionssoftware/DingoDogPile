691769503d72d45947169cef424cd79f345f5f5c
pbriggs <p.ivan.briggs@gmail.com>
2014-04-07 01:16:03 -0700

    Updated Shooter RigidBody2D

--- Assets/Scripts/Weapons/Shooter.cs
+++ Assets/Scripts/Weapons/Shooter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
-    public Rigidbody2D rocket;
+    public Rigidbody2D bullet;
     public float speed = 20f;
 
     private PlayerController player;
@@ -25,13 +25,13 @@ public class Shooter : MonoBehaviour {
 
             if (player.isFacingRight)
             {
-                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
-                bulletInstance.velocity = new Vector2(speed, 0);
+                Rigidbody2D 'Instance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+                'Instance.velocity = new Vector2(speed, 0);
             }
             else 
             {
-                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
-                bulletInstance.velocity = new Vector2(-speed, 0);                    
+                Rigidbody2D 'Instance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
+                'Instance.velocity = new Vector2(-speed, 0);                    
             }
         }    
     }
